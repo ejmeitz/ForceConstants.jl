65043bde7911f79315b804a60aef14775c52201b
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-05-31 12:13:24 -0400

    Update third_order.jl

--- src/third_order.jl
+++ src/third_order.jl
@@ -107,6 +107,7 @@ function save_third_order(Ψ, N_atoms, D, outpath; filename = "third_order", fmt
     if fmt == :txt
         filepath = joinpath(outpath, filename*".txt")
         f = open(filepath, "w")
+        println("$(N_atoms) atoms, Sum Ψ = $(ustrip(sum(Ψ))), Units: $(unit(Ψ[1,1,1]))")
         for i in range(1, N_atoms)
             for j in range(1, N_atoms)
                 for k in range(1,N_atoms)
