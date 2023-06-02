b481f5e41544515b85f8b6f6955d284728aef2a6
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-06-02 15:28:55 -0400

    dispersion plots work!

--- src/dispersion.jl
+++ src/dispersion.jl
@@ -1,45 +1,60 @@
 export get_dispersion_points
 
-function get_dispersion_points(sys::UnitCellSystem{3}, pot::PairPotential; directions = ([1.0, 0.0, 0.0],);
-                                    group_by = :branch, tol = 1e-12, unit_system = :REAL)
+function get_dispersion_points(sys::UnitCellSystem{3}, pot::PairPotential;
+                 directions = ([1.0, 0.0, 0.0],), group_by = :branch, tol = 1e-12, unit_system = :REAL)
     
-    @assert group_by ∈ [:branch, :k_point] "group_by can only be :branch or :k_point"
+    @assert (group_by ∈ [:branch, :k_point]) "group_by can only be :branch or :k_point"
 
     Δk = (2*pi)./sys.box_sizes_SC
     left_zone_edges = (-pi./sys.box_sizes_UC) .+ Δk
     right_zone_edges = pi./sys.box_sizes_UC
 
-    k_points = []
-    k_current = ustrip.([left_zone_edges[1],0,0])
-        # direction ./= norm(direction) #convert to unit vector
-        
-    #This is just 1,0,0 direction for now
-    for i in range(left_zone_edges[1], right_zone_edges[1], step = Δk[1])
-        push!(k_points, copy(k_current))
-        k_current .+= [ustrip(Δk[1]),0,0]
+    #List of all k_points
+    # k_points = [left_zone_edges .+ (Δk*(i-1)) for i in sys.num_unit_cells]
+
+    @assert all(sys.num_unit_cells[1] .== sys.num_unit_cells) "Current implementation assumes same number of units cells in each direction"
+    out = Dict()
+    for direction in directions
+        k_points = []
+        k_current = left_zone_edges .* direction
+        for i in range(1,sys.num_unit_cells[1])
+            push!(k_points, copy(k_current))
+            k_current .+= (Δk .* direction)
+        end
+
+        ω_all = Dict()
+        for k_point in k_points
+            dynmat_uc = dynamicalMatrix(sys, pot, SVector{3}(k_point), tol)
+            ω_sq = eigen(Hermitian(ustrip.(dynmat_uc))).values;
+            if group_by == :k_point
+                ω_all[k_point] = ω_sq
+            elseif group_by == :branch
+                throw(error("Unimplemented"))
+            end
+        end
+        out[tuple(direction...)] = ω_all
     end
 
-    ω_all = Dict()
-    println(k_points)
-    for k_point in k_points
-        dynmat_uc = dynamicalMatrix(sys, pot, SVector{3}(k_point), tol)
-        ω_sq = eigen(Hermitian(ustrip.(dynmat_uc))).values;
-        ω_all[k_point] = ω_sq
-    end
+
+
+
     #Sqrt and convert units on freqs
-    for k in keys(ω_all)
+    for b in keys(out)
+        ω_all = out[b]
+        for k in keys(ω_all)
 
-        if ustrip.(k) == [0.0, 0.0, 0.0] #if at origin set rigid translation modes to 0
-            idxs = sortperm(abs.(ω_all[k]))
-            ω_all[k][idxs[1:3]] .= 0.0
-        end
+            if ustrip.(k) == [0.0, 0.0, 0.0] #if at origin set rigid translation modes to 0
+                idxs = sortperm(abs.(ω_all[k]))
+                ω_all[k][idxs[1:3]] .= 0.0
+            end
 
-        if unit_system == :REAL
-            ω_all[k] = sqrt.(ω_all[k] .* 4184 .* 1000 .* 1e20)./(2*pi*1e12)
-        else
-            throw(error("Only real units supported at this time"))
+            if unit_system == :REAL
+                ω_all[k] = sqrt.(ω_all[k] .* 4184 .* 1000 .* 1e20)./(2*pi*1e12)
+            else
+                throw(error("Only real units supported at this time"))
+            end
         end
     end
 
-    return ω_all
+    return out
 end
