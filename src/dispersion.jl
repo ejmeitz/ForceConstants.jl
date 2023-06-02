facdb48aed8b8a1e3237ace658651e3046264c08
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-06-02 16:37:20 -0400

    tf

--- src/dispersion.jl
+++ src/dispersion.jl
@@ -7,17 +7,13 @@ function get_dispersion_points(sys::UnitCellSystem{3}, pot::PairPotential;
 
     Δk = (2*pi)./sys.box_sizes_SC
     left_zone_edges = (-pi./sys.box_sizes_UC) .+ Δk
-    right_zone_edges = pi./sys.box_sizes_UC
-
-    #List of all k_points
-    # k_points = [left_zone_edges .+ (Δk*(i-1)) for i in sys.num_unit_cells]
 
     @assert all(sys.num_unit_cells[1] .== sys.num_unit_cells) "Current implementation assumes same number of units cells in each direction"
     out = Dict()
     for direction in directions
         k_points = []
         k_current = left_zone_edges .* direction
-        for i in range(1,sys.num_unit_cells[1])
+        for i in range(1,sys.num_unit_cells[1]) #this is where cubic assumption matters
             push!(k_points, copy(k_current))
             k_current .+= (Δk .* direction)
         end
@@ -27,7 +23,7 @@ function get_dispersion_points(sys::UnitCellSystem{3}, pot::PairPotential;
             dynmat_uc = dynamicalMatrix(sys, pot, SVector{3}(k_point), tol)
             ω_sq = eigen(Hermitian(ustrip.(dynmat_uc))).values;
             if group_by == :k_point
-                ω_all[k_point] = ω_sq
+                ω_all[ustrip(k_point)] = ω_sq
             elseif group_by == :branch
                 throw(error("Unimplemented"))
             end
@@ -49,6 +45,8 @@ function get_dispersion_points(sys::UnitCellSystem{3}, pot::PairPotential;
             end
 
             if unit_system == :REAL
+                println(k)
+                println(ω_all[k])
                 ω_all[k] = sqrt.(ω_all[k] .* 4184 .* 1000 .* 1e20)./(2*pi*1e12)
             else
                 throw(error("Only real units supported at this time"))
