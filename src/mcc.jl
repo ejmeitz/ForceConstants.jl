ba4130afa868869eb3a0474cefec49da6970d97e
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2023-08-11 12:21:30 -0400

    remove tols from MCC3 calculation

--- src/mcc.jl
+++ src/mcc.jl
@@ -5,7 +5,7 @@ export mcc3
 Converts third order forces constants, `Ψ` into third order modal coupling constants (MCC).
 Does not divide MCC calculation into smaller chunks. This might exhaust GPU memory.
 """
-function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, tol = 1f-12)
+function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2})
 
     K3 = CUDA.zeros(Float32, size(Ψ));
 
@@ -13,15 +13,7 @@ function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, tol = 1f-12)
         K3[n,m,l] = Ψ[i,j,k] * phi[i,n] * phi[j,m] * phi[k,l]
     end
 
-    K3_CPU = Array(K3)
-
-    for i in eachindex(K3_CPU)
-        if abs(K3_CPU[i]) < tol
-            K3_CPU[i] = 0.0f32
-        end
-    end
-
-    return K3_CPU
+    return Array(K3)
 end
 
 """
@@ -31,7 +23,7 @@ parameter `block_size` specifies problem size when calculating the MCC. For exam
 The `phi` matrix will be automatically truncated to adjust for this.
     Try to maximize `block_size` and make it a power of 2.
 """
-function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, block_size::Int; tol = 1.0f-12)
+function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, block_size::Int)
 
     @assert size(phi)[1] % block_size == 0
     @assert block_size > 0
@@ -44,7 +36,6 @@ function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, block_size::Int
     #Pre-allocate GPU storage to re-use
     K3_GPU_block = CUDA.zeros(Float32, (block_size, block_size, block_size))
 
-    #Only calculate lower half of K3
     for i in 1:n_blocks_per_dim
         for j in 1:n_blocks_per_dim
             for k in 1:n_blocks_per_dim
@@ -54,38 +45,13 @@ function mcc3(Ψ::CuArray{Float32, 3}, phi::CuArray{Float32, 2}, block_size::Int
                 K3_GPU_block =  mcc3_blocked!(K3_GPU_block, Ψ, phi, [dim1_range, dim2_range, dim3_range])
                 
                 copyto!(tmp, K3_GPU_block) #TODO can i remove this? uses scalar indexing to copy directly into big array
-                copyto!(view(K3_CPU,dim1_range, dim2_range, dim3_range), tmp)
+                copyto!(view(K3_CPU, dim1_range, dim2_range, dim3_range), tmp)
             end
         end
     end
 
     #TODO this calculates more than it needs to atm, can only calculate unique part and rotate 
     #TODO to enforce symmetry, kinda annoying to implement, will matter more when system > GPU RAM
-    #Enforce symmetry of K3
-    # for i in 1:n_blocks_per_dim
-    #     for j in 1:i
-    #         for k in 1:j
-    #             idx_combos = multiset_permutations([i,j,k], 3)
-    #             filter!(p -> p != [i,j,k] , collect(idx_combos))
-    #             dim1_range_orig = (block_size*(i-1) + 1):(block_size*i)
-    #             dim2_range_orig = (block_size*(j-1) + 1):(block_size*j)
-    #             dim3_range_orig = (block_size*(k-1) + 1):(block_size*k)
-    #             for idx in idx_combos
-    #                 dim1_range = (block_size*(idx[1]-1) + 1):(block_size*idx[1])
-    #                 dim2_range = (block_size*(idx[2]-1) + 1):(block_size*idx[2])
-    #                 dim3_range = (block_size*(idx[3]-1) + 1):(block_size*idx[3])
-    #                 copyto!(view(K3_CPU, dim1_range, dim2_range, dim3_range),
-    #                         view(K3_CPU, dim1_range_orig, dim2_range_orig, dim3_range_orig))
-    #             end
-    #         end
-    #     end
-    # end
-
-    for i in eachindex(K3_CPU)
-        if abs(K3_CPU[i]) < tol
-            K3_CPU[i] = 0.0f32
-        end
-    end
 
     return K3_CPU
 end
